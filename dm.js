8a1629a0be52a715e5d0f77cc98537f01322187b
Donald Boncodin <boncodindonald6@gmail.com>
2023-10-08 21:23:24 +0800

    ©️ GO WATCH 2023 ALL RIGTH RESERVED

---
+++ dm.js
@@ -0,0 +1,17 @@
+const darkModeToggle = document.getElementById('dark-mode-toggle-input');
+const body = document.body;
+
+// Check if dark mode is enabled in localStorage
+const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
+
+// Set initial dark mode state based on localStorage
+if (isDarkMode) {
+    body.classList.add('dark-mode');
+    darkModeToggle.checked = true;
+}
+
+// Toggle dark mode and save the preference in localStorage
+darkModeToggle.addEventListener('change', () => {
+    body.classList.toggle('dark-mode');
+    localStorage.setItem('darkMode', body.classList.contains('dark-mode') ? 'enabled' : 'disabled');
+});
