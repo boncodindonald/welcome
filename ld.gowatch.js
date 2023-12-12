02d7f400026e4f1d3d075891c8278a353d15e8f7
Donald Boncodin <91857823+go-watch@users.noreply.github.com>
2023-12-12 21:12:45 +0800

    Update ld.gowatch.js
    Signed-off-by: Donald Boncodin <91857823+go-watch@users.noreply.github.com>

--- ld.gowatch.js
+++ ld.gowatch.js
@@ -1,6 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function () {
-        document.querySelector(".loading-screen").style.display = "none";
-        document.body.style.overflow = "auto";
-    }, 2000);
-});
+document.addEventListener("DOMContentLoaded", function () {setTimeout(function () {document.querySelector(".loading-screen").style.display = "none";document.body.style.overflow = "auto";}, 2000);});
