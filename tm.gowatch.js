bd1986bf8a32ececa0c4ac2dbdcac51cd98003b0
Donald Boncodin <91857823+go-watch@users.noreply.github.com>
2023-12-12 21:18:40 +0800

    Update tm.gowatch.js
    Signed-off-by: Donald Boncodin <91857823+go-watch@users.noreply.github.com>

--- tm.gowatch.js
+++ tm.gowatch.js
@@ -1,14 +1,1 @@
-const darkModeToggle = document.getElementById('tm');
-const body = document.body;
-
-const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
-
-if (isDarkMode) {
-    body.classList.add('dark-mode');
-    darkModeToggle.checked = true;
-}
-
-darkModeToggle.addEventListener('change', () => {
-    body.classList.toggle('dark-mode');
-    localStorage.setItem('darkMode', body.classList.contains('dark-mode') ? 'enabled' : 'disabled');
-});
+const darkModeToggle = document.getElementById('tm');const body = document.body;const isDarkMode = localStorage.getItem('darkMode') === 'enabled';if (isDarkMode) {body.classList.add('dark-mode');darkModeToggle.checked = true;}darkModeToggle.addEventListener('change', () => {body.classList.toggle('dark-mode');localStorage.setItem('darkMode', body.classList.contains('dark-mode') ? 'enabled' : 'disabled');});
