641a81f1a8149f12c0d14a59a19857b8d7384777
Donald Boncodin <91857823+go-watch@users.noreply.github.com>
2023-12-21 18:14:52 +0800

    Add files via upload
    Signed-off-by: Donald Boncodin <91857823+go-watch@users.noreply.github.com>

--- tm.gowatch.js
+++ tm.gowatch.js
@@ -1,1 +1,17 @@
-const darkModeToggle = document.getElementById('tm');const body = document.body;const isDarkMode = localStorage.getItem('darkMode') === 'enabled';if (isDarkMode) {body.classList.add('dark-mode');darkModeToggle.checked = true;}darkModeToggle.addEventListener('change', () => {body.classList.toggle('dark-mode');localStorage.setItem('darkMode', body.classList.contains('dark-mode') ? 'enabled' : 'disabled');});
+const darkModeToggle = document.getElementById('tm');
+const body = document.body;
+
+// Check if dark mode is enabled in localStorage
+const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
+
+// Set initial dark mode state based on localStorage
+if (isDarkMode) {
+    body.classList.add('dark-mode');
+    darkModeToggle.checked = true;
+}
+
+// Toggle dark mode and save the preference in localStorage
+darkModeToggle.addEventListener('change', () => {
+    body.classList.toggle('dark-mode');
+    localStorage.setItem('darkMode', body.classList.contains('dark-mode') ? 'enabled' : 'disabled');
+});
