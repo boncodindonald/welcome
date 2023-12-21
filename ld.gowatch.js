fa95e92634b7872351f280f6ce0a0ba86a07b70b
Donald Boncodin <91857823+go-watch@users.noreply.github.com>
2023-12-21 20:48:47 +0800

    ©️ GO WATCH 2023 ALL RIGTH RESERVED
    Signed-off-by: Donald Boncodin <91857823+go-watch@users.noreply.github.com>

--- ld.gowatch.js
+++ ld.gowatch.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Simulate loading time (you can remove this in a real application)
+    
     setTimeout(function () {
         document.querySelector(".loading-screen").style.display = "none";
-        document.body.style.overflow = "auto"; // Show scrollbars
-    }, 2000); // Adjust the delay as needed
+        document.body.style.overflow = "auto";
+    }, 2000);
 });
--- tm.gowatch.js
+++ tm.gowatch.js
@@ -1,16 +1,16 @@
 const darkModeToggle = document.getElementById('tm');
 const body = document.body;
 
-// Check if dark mode is enabled in localStorage
+
 const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
 
-// Set initial dark mode state based on localStorage
+
 if (isDarkMode) {
     body.classList.add('dark-mode');
     darkModeToggle.checked = true;
 }
 
-// Toggle dark mode and save the preference in localStorage
+
 darkModeToggle.addEventListener('change', () => {
     body.classList.toggle('dark-mode');
     localStorage.setItem('darkMode', body.classList.contains('dark-mode') ? 'enabled' : 'disabled');
